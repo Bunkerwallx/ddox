aefb70ad0982efb712ed4c9b879929bb8171bf7d
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-02-28 17:37:42 -0600

    Actualizar termux

---
+++ termux/setup.sh
@@ -0,0 +1,10 @@
+apt update && apt upgrade
+pkg install python3 
+pkg install python2
+pip install colorama
+pip install re
+pip install virtualenv
+pip install env
+pip install threading
+pip install sys
+pip install random
